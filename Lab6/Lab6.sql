fe28c63e3714a8d1db39f1763d5f88557155cff0
FrankSiderio <franksiderio@yahoo.com>
2016-02-29 22:53:52 -0500

    Did number 7

--- Lab6/Lab6.sql
+++ Lab6/Lab6.sql
@@ -28,9 +28,11 @@ order by orders.totalusd desc
 
 --4 Shows all the customer names (in alphabetical order) and their total ordered
 
-select *
-from customers 
-
+select customers.name, coalesce(sum(orders.qty), 0) 
+from customers
+left outer join orders on customers.cid = orders.cid
+group by customers.name
+order by customers.name asc
 
 --5 Shows the names of all the customers who bought products from agents based in Tokyo along with the
 --  names of the products they ordered, and the names of the agents who sold it to them
@@ -51,5 +53,22 @@ from orders, customers, products
 where orders.cid = customers.cid
   and orders.pid = products.pid
   and orders.totalusd != (products.priceusd * orders.qty * ((100 - customers.discount) / 100));
+
+--7 Difference between left and right outer joins
+-- A left outer join returns all the rows from the left table with the matches from the right table
+
+select customers.cid
+from orders
+left outer join customers on orders.cid = customers.cid;
+--This is a good example because it lists all the cids in the orders table. c005 never made an order so 
+--it is not listed. 
+
+-- A right outer join returns all the rows from the right table with matches from the left table 
+select customers.cid
+from customers
+right outer join orders on customers.cid = orders.cid;
+--This is similar to the last one but using a right outer join. It returns all the matches from the right
+--table(orders) and left table(customers) again c005 is not shown because it never made an order so it is 
+--not a match
   
 
